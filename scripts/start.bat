67691c7ba0f47ae10b43d47b00c0918872d5fd46
jaya2 <jaya2004kra@gmail.com>
2024-06-04 12:15:48 +0530

    modified url

--- scripts/start.bat
+++ scripts/start.bat
@@ -3,7 +3,7 @@ REM Run the Python script
 python "..\data_to_powerbi_main.py"
 
 REM Open the web browser to a specific URL
-python -c "import webbrowser; webbrowser.open('https://app.powerbi.com/groups/me/dashboards/1b1a4654-3041-4154-b269-cad399473e79?experience=power-bi')"
+python -c "import webbrowser; webbrowser.open('{add_power_bi_dashboard_url}')"
 
 REM Pause to keep the window open
 pause
