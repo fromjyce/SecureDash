7d75d120b556defa377738d548e72bdba5318347
jaya2 <jaya2004kra@gmail.com>
2024-06-05 11:23:53 +0530

    added disabled

--- renderer.js
+++ renderer.js
@@ -1,21 +1,24 @@
 const { ipcRenderer } = require("electron");
 
 window.onload = () => {
-  
   const playButton = document.getElementById("play-button");
   const stopButton = document.getElementById("stop-button");
+  const checkPowerBiButton = document.getElementById("check-power-bi");
 
   stopButton.classList.add("disabled");
+  checkPowerBiButton.disabled = true;
 
   window.startPython = () => {
     ipcRenderer.send("start-python");
     playButton.classList.add("disabled");
     stopButton.classList.remove("disabled");
+    checkPowerBiButton.disabled = false;
   };
 
   window.stopPython = () => {
     ipcRenderer.send("stop-python");
     stopButton.classList.add("disabled");
     playButton.classList.remove("disabled");
+    checkPowerBiButton.disabled = true;
   };
 };
