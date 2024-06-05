e2a10fea46383fbe9fcee912b610c15d03102cfc
jaya2 <jaya2004kra@gmail.com>
2024-06-05 09:08:33 +0530

    added image buttons

--- renderer.js
+++ renderer.js
@@ -1,9 +1,21 @@
 const { ipcRenderer } = require("electron");
 
-function startPython() {
-  ipcRenderer.send("start-python");
-}
+window.onload = () => {
+  
+  const playButton = document.getElementById("play-button");
+  const stopButton = document.getElementById("stop-button");
 
-function stopPython() {
-  ipcRenderer.send("stop-python");
-}
+  stopButton.classList.add("disabled");
+
+  window.startPython = () => {
+    ipcRenderer.send("start-python");
+    playButton.classList.add("disabled");
+    stopButton.classList.remove("disabled");
+  };
+
+  window.stopPython = () => {
+    ipcRenderer.send("stop-python");
+    stopButton.classList.add("disabled");
+    playButton.classList.remove("disabled");
+  };
+};
