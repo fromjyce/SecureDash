197653c744961eae13b9c28acb146933f3282435
jaya2 <jaya2004kra@gmail.com>
2024-06-05 12:47:14 +0530

    sql

--- scripts/securedash_sql_script.sql
+++ scripts/securedash_sql_script.sql
@@ -1,3 +1,26 @@
 CREATE DATABASE securedash;
 
 use securedash;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE users ADD COLUMN name VARCHAR(255) NOT NULL;
+ALTER TABLE users ADD COLUMN email VARCHAR(255) NOT NULL UNIQUE;
+
+CREATE TABLE packet_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    source_ip VARCHAR(45),
+    destination_ip VARCHAR(45),
+    timestamp DATETIME,
+    status VARCHAR(10),
+    flow_duration INT,
+    protocol INT,
+    total_fwd_packets INT,
+    total_backward_packets INT
+);
+
+SELECT * from packet_data; 
