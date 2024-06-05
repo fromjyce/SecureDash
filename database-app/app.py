563a46e213a57fb19f24d25a148b333304e08f18
jaya2 <jaya2004kra@gmail.com>
2024-06-05 16:14:26 +0530

    added

--- database-app/app.py
+++ database-app/app.py
@@ -13,20 +13,23 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    order = request.args.get('order', 'desc')
+    sort_order = 'DESC' if order == 'desc' else 'ASC'
+    
     if request.method == 'POST':
         start_timestamp = request.form.get('start_timestamp')
         end_timestamp = request.form.get('end_timestamp')
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM packet_data WHERE timestamp BETWEEN %s AND %s ORDER BY timestamp DESC", (start_timestamp, end_timestamp))
+        cur.execute(f"SELECT * FROM packet_data WHERE timestamp BETWEEN %s AND %s ORDER BY timestamp {sort_order}", (start_timestamp, end_timestamp))
         data = cur.fetchall()
         cur.close()
-        return render_template('index.html', data=data)
+        return render_template('index.html', data=data, order=order)
     else:
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM packet_data ORDER BY timestamp DESC")
+        cur.execute(f"SELECT * FROM packet_data ORDER BY timestamp {sort_order}")
         data = cur.fetchall()
         cur.close()
-        return render_template('index.html', data=data)
+        return render_template('index.html', data=data, order=order)
 
 if __name__ == '__main__':
     app.run(debug=True)
