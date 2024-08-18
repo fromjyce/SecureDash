e7933140fbf94625b52e8bc34046716d444c0bd4
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2024-08-18 09:34:49 +0530

    updated

--- database-app/app.py
+++ database-app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Jayas1709#'
+app.config['MYSQL_PASSWORD'] = '###'
 app.config['MYSQL_DB'] = 'securedash'
 
 mysql = MySQL(app)
